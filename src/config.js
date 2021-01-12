be760edd5c1187d805b65c3c1d7047e04e00289a
SuNiL-Chau <1234huts@gmail.com>
2021-01-12 22:46:30 +0530

    First commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const clientId = "f8980ad9d7774c038bd9bd8bb26f3a39";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
